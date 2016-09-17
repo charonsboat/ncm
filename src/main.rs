57c065fffdbbf097cdfeb8fbcbce9fd3b33d1717
drm2 <davidrmyersii@gmail.com>
2016-09-16 22:11:28 -0400

    Implementing Clap.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#[macro_use] // implement macros from clap to allow loading from YAML
+extern crate clap;
+
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let config  = load_yaml!("nxconf.yml");
+    let matches = App::from_yaml(config).get_matches();
 }
